713cba4770a51a649480adb0587b11b362d2cab9
paramashiva123 <kambalaparamashiva123@gmail.com>
2025-06-03 19:18:26 +0530

    code updated

--- frontend.py
+++ frontend.py
@@ -1,39 +1,31 @@
-# if you dont use pipenv uncomment the following:
-# from dotenv import load_dotenv
-# load_dotenv()
-
-
-#Step1: Setup UI with streamlit (model provider, model, system prompt, web_search, query)
 import streamlit as st
+import requests
 
 st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
 st.title("AI Chatbot Agents")
 st.write("Create and Interact with the AI Agents!")
 
-system_prompt=st.text_area("Define your AI Agent: ", height=70, placeholder="Type your system prompt here...")
+system_prompt = st.text_area("Define your AI Agent: ", height=70, placeholder="Type your system prompt here...")
 
 MODEL_NAMES_GROQ = ["llama-3.3-70b-versatile", "mixtral-8x7b-32768"]
 MODEL_NAMES_OPENAI = ["gpt-4o-mini"]
 
-provider=st.radio("Select Provider:", ("Groq", "OpenAI"))
+provider = st.radio("Select Provider:", ("Groq", "OpenAI"))
 
 if provider == "Groq":
     selected_model = st.selectbox("Select Groq Model:", MODEL_NAMES_GROQ)
 elif provider == "OpenAI":
     selected_model = st.selectbox("Select OpenAI Model:", MODEL_NAMES_OPENAI)
 
-allow_web_search=st.checkbox("Allow Web Search")
+allow_web_search = st.checkbox("Allow Web Search")
 
-user_query=st.text_area("Enter your query: ", height=150, placeholder="Ask Anything!")
+user_query = st.text_area("Enter your query: ", height=150, placeholder="Ask Anything!")
 
-API_URL="http://127.0.0.1:9999/chat"
+API_URL = "http://127.0.0.1:9999/chat"
 
 if st.button("Ask Agent!"):
     if user_query.strip():
-        #Step2: Connect with backend via URL
-        import requests
-
-        payload={
+        payload = {
             "model_name": selected_model,
             "model_provider": provider,
             "system_prompt": system_prompt,
@@ -41,7 +33,8 @@ if st.button("Ask Agent!"):
             "allow_search": allow_web_search
         }
 
-        response=requests.post(API_URL, json=payload)
+        response = requests.post(API_URL, json=payload)
+
         if response.status_code == 200:
             response_data = response.json()
             if "error" in response_data:
@@ -49,6 +42,5 @@ if st.button("Ask Agent!"):
             else:
                 st.subheader("Agent Response")
                 st.markdown(f"**Final Response:** {response_data}")
-
-
-
+        else:
+            st.error("Failed to connect to the AI backend.")
